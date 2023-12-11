ff0c3b107d0edcce9d0f8760bdd22fb9f179b0d8
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2023-12-11 15:08:53 -0300

    Refactoring. movineg CreateThoughtComponent to correct folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
-import { CreateThoughtComponent } from './components/thougths/create-thought/create-thought.component';
+import { CreateThoughtComponent } from './components/thoughts/create-thought/create-thought.component';
 import { ListThoughtsComponent } from './components/thoughts/list-thoughts/list-thoughts.component';
 
 @NgModule({
