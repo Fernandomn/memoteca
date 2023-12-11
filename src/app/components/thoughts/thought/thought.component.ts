9d0cb9d36b65eef3fc7f16a13c61d8bae0c0b840
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2023-12-11 16:12:43 -0300

    receiving Thought data by input in ThoughtComponent, and creating formatting method

--- src/app/components/thoughts/thought/thought.component.ts
+++ src/app/components/thoughts/thought/thought.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Thought } from 'src/app/interfaces/thoughts';
 
 @Component({
@@ -7,9 +7,18 @@ import { Thought } from 'src/app/interfaces/thoughts';
   styleUrls: ['./thought.component.css'],
 })
 export class ThoughtComponent {
-  thought: Thought = {
+  @Input() thought: Thought = {
     content: 'I love Angular',
     author: 'Nay',
     model: 'modelo3',
   };
+
+  constructor() {}
+
+  thoughtLength(): string {
+    if (this.thought.content && this.thought.content.length >= 256) {
+      return 'pensamento-g';
+    }
+    return 'pensamento-p';
+  }
 }
