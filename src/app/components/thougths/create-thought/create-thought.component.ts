d0c2a10c5e19d1be82d3621c8c57c0104ea8e20e
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2023-12-11 15:04:24 -0300

    updating createThoughtComponent to use 2-way data binding

--- src/app/components/thougths/create-thought/create-thought.component.ts
+++ src/app/components/thougths/create-thought/create-thought.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-create-thought',
   templateUrl: './create-thought.component.html',
-  styleUrls: ['./create-thought.component.css']
+  styleUrls: ['./create-thought.component.css'],
 })
 export class CreateThoughtComponent {
+  thought = {
+    id: '1',
+    content: 'aprendendo Angular',
+    author: 'Dev',
+    model: '',
+  };
 
+  createThougth() {
+    alert('novo pensamento criado');
+  }
+
+  cancel() {
+    alert('cancelado');
+  }
 }
