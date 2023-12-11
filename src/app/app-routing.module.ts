fcd20d226ab8d9e7698d007b67c5e2dce4c7cfbc
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2023-12-11 15:48:37 -0300

    implementing routes, and using RoutingModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateThoughtComponent } from './components/thoughts/create-thought/create-thought.component';
+import { ListThoughtsComponent } from './components/thoughts/list-thoughts/list-thoughts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'listarPensamento', pathMatch: 'full' },
+  { path: 'criarPensamento', component: CreateThoughtComponent },
+  { path: 'listarPensamento', component: ListThoughtsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
