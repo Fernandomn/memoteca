a715f0382262e702a36b423d7840d8e70ae34c27
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2023-12-11 13:33:11 -0300

    creating Header, Footer and CreateThought components. Placing Header and Footer in main screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './compents/header/header.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CreateThoughtComponent } from './components/thougths/create-thought/create-thought.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    CreateThoughtComponent
   ],
   imports: [
     BrowserModule,
